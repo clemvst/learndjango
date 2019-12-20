49bb1be86c9be37205d1dff6f9b04e2dfd2a5f72
Clemence <clemence.vast@mines-nancy.org>
2019-12-20 16:20:02 +0100

    adding models

--- disquaire_project/store/models.py
+++ disquaire_project/store/models.py
@@ -1,16 +1,36 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
-ARTISTS = {
-  'francis-cabrel': {'name': 'Francis Cabrel'},
-  'lej': {'name': 'Elijay'},
-  'rosana': {'name': 'Rosana'},
-  'maria-dolores-pradera': {'name': 'María Dolores Pradera'},
-}
+# # Create your models here.
+# ARTISTS = {
+#   'francis-cabrel': {'name': 'Francis Cabrel'},
+#   'lej': {'name': 'Elijay'},
+#   'rosana': {'name': 'Rosana'},
+#   'maria-dolores-pradera': {'name': 'María Dolores Pradera'},
+# }
+#
+#
+# ALBUMS = [
+#   {'name': 'Sarbacane', 'artists': [ARTISTS['francis-cabrel']]},
+#   {'name': 'La Dalle', 'artists': [ARTISTS['lej']]},
+#   {'name': 'Luna Nueva', 'artists': [ARTISTS['rosana'], ARTISTS['maria-dolores-pradera']]}
+# ]
+class Contact(models.model):
+    email = models.EmailField(max_length = 100)
+    name = models.CharField(max_length = 200)
 
+class Artist(models.model):
+    name = models.CharField(max_length = 200, unique = True)
 
-ALBUMS = [
-  {'name': 'Sarbacane', 'artists': [ARTISTS['francis-cabrel']]},
-  {'name': 'La Dalle', 'artists': [ARTISTS['lej']]},
-  {'name': 'Luna Nueva', 'artists': [ARTISTS['rosana'], ARTISTS['maria-dolores-pradera']]}
-]
+class Album(models.model):
+    reference = models.IntegerField(null = True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    available = models.BooleanField(default = True)
+    title = models.CharField(max_length = 200)
+    picture = models.URLField()
+
+class Booking(models.model):
+    created_at = models.DateTimeField(auto_now_add = True)
+    contacted = models.BooleanField(default = False)
+
+class AlbumArtist(models.model):
+    id_Artist = Artist.name
--- disquaire_project/store/urls.py
+++ disquaire_project/store/urls.py
@@ -6,4 +6,5 @@ from django.urls import path
 urlpatterns = [
     url(r'^$', views.listing),
     url(r'^(?P<album_id>[0-9]+)/$', views.detail),
+    url(r'^search/$', views.search),
 ]
